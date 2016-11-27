69f98c22a7393c00cd8468d259edc0094d4d7fda
zerooneat <zerooneat@gmail.com>
2016-11-27 19:30:55 +0700

    Initial commit

---
+++ app/src/main/java/com/example/barang_hkh/driapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.barang_hkh.driapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/barang_hkh/driapp/MainLayout.java
@@ -0,0 +1,13 @@
+package com.example.barang_hkh.driapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_layout);
+    }
+}
